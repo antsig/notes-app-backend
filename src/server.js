4ddd4609fb996a9f8cf595b39677ed16631f231d
Sigid Priharsanto <underfighterkkh@gmail.com>
2024-04-07 01:09:43 +0800

    add

--- src/server.js
+++ src/server.js
@@ -1,10 +1,15 @@
 const Hapi = require('@hapi/hapi');
 const routes = require('./routes');
- 
+
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
     host: 'localhost',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
   });
  
   await server.start();
